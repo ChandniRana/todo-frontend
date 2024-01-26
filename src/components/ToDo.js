298d4c316b55987acfdb6732115d2f42a8e8b73f
Chandni Rana <91819256+ChandniRana@users.noreply.github.com>
2024-01-26 10:51:51 +0530

    Add files via upload

---
+++ src/components/ToDo.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { BiEdit } from 'react-icons/bi';
+import { AiFillDelete } from 'react-icons/ai';
+
+const ToDo = ({ text, updateMode, deleteToDo }) => {
+  const [completed, setCompleted] = useState(false);
+
+  const handleUpdate = () => {
+    updateMode();
+  };
+
+  const handleComplete = () => {
+    setCompleted(!completed);
+  };
+
+  return (
+    <div className={`todo ${completed ? 'completed' : ''}`}>
+      <div className="text">{text}</div>
+      <div className="icons">
+        <button className='complete-button' onClick={handleComplete}>
+          {completed ? 'Undo Complete' : 'Complete'}
+        </button>
+        <button className='update-button' onClick={handleUpdate}>Update</button>
+        <button className='delete-button' onClick={deleteToDo}>Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default ToDo;
